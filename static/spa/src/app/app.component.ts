724172339169f4436a59b61bac37a3257b1b55e6
jiniux <53437303+jiniux@users.noreply.github.com>
2025-02-18 13:00:41 +0100

    Fix leftover

--- static/spa/src/app/app.component.ts
+++ static/spa/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   constructor(private readonly cartService: CartService) {}
   
   ngOnInit(): void {
-    this.cartService.
+    this.cartService.reloadCart();
   }
 
   title = 'aap';
